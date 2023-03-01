6d5bb928701c6ebf8236fab68ca9f9ffac2bee25
SEE <isip_e.e.sibgatullina@mpt.ru>
2023-03-01 11:37:23 +0300

    dssd

---
+++ Session1_XX/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Session1_XX.DataSet1TableAdapters;
+using System.Windows;
+
+namespace Session1_XX
+{
+
+    public partial class MainWindow : Window
+    {
+        DataSet1 dataset;
+        UsersTableAdapter usersTA;
+        QueriesTableAdapter authTA;
+        public MainWindow()
+        {
+            InitializeComponent();
+            usersTA = new UsersTableAdapter();
+            authTA = new QueriesTableAdapter();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (AuthRole(TxtUsername.Text, TxtPassword.Password.ToString())>0)
+            {
+                
+            }
+        }
+        private int? AuthRole (string login, string password)
+        {
+            int? role = authTA.Auth(login, password);
+            return role;
+        }
+
+
+    }
+}
